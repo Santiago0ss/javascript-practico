d60d9b0d9f6ec73eda95a194d9cdae2a4f575d21
Santiago Salazar <santiago1234ocampo@gmail.com>
2021-12-16 21:16:49 -0300

    mediana2

--- mediana.js
+++ mediana.js
@@ -1,3 +1,17 @@
+function calcularMediaAritmetica(lista) {
+
+    const sumaLista = lista.reduce(
+        function (valorAcumulado = 0, nuevoElemento) {
+
+            return valorAcumulado + nuevoElemento;
+        }
+    );
+
+    const promedioLista = sumaLista / lista.length;
+
+    return promedioLista;
+}
+
 const lista1 = [
     100,
     200,
@@ -5,7 +19,7 @@ const lista1 = [
     4000000,
 ];
 
-const mitadLista1 = lista1.length / 2;
+const mitadLista1 = parseInt(lista1.length / 2);
 
 function esPar(numerito) {
     if (numerito % 2 === 0) {
@@ -18,6 +32,15 @@ function esPar(numerito) {
 let mediana;
 
 if (esPar(lista1.length)){
+    const elemento1 = lista1[mitadLista1 - 1];
+    const elementon2 = lista1[mitadLista1];
+
+    const promedioElemento1y2 = calcularMediaAritmetica([
+        elemento1,
+        elementon2,
+    ]);
+
+    mediana = promedioElemento1y2;
 
 } else {
     mediana = lista1[mitadLista1];
